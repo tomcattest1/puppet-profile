bbcf8ae9a040033fa30e1b5ac695686872c1a848
root <root@shashiudawa5.mylabserver.com>
2018-01-21 20:07:38 +0000

    deploy

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -1,9 +1,10 @@
 class profile::tomcat(
-  $deploy_url = "https://9-118298370-gh.circle-artifacts.com/0/tmp/circle-artifacts.jxRQwEe/sysfoo.war",
-  $checksum_value = "de01db71b4e67f17481665dda4b802b7",
+  String $deploy_url,
+  String $checksum_value,
+  Array $deploy_name,
 ) {
   include ::tomcat
-  tomcat::deploy { 'suresh': 
+  tomcat::deploy { $deploy_name: 
     deploy_url      => $deploy_url,
     checksum_value  => $checksum_value,
   }
